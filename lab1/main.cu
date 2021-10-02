b7a41d583c8ea4783b5674b21025f2963367349d
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-02 16:46:12 -0700

    make clean manual

--- lab1/main.cu
+++ lab1/main.cu
@@ -1,49 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "timing.h"
-#include "prime.h"
-
-
-int main (int argc, const char * argv[]) {    
-   
-   if(argc < 2)
-   {
-       printf("Usage: prime upbound\n");
-       exit(-1);
-   }
-   bignum N = (bignum) atoi(argv[1]);
-   if(N <= 0)
-   {
-       printf("Usage: prime upbound, you input invalid upbound number!\n");
-       exit(-1);
-   }
-   
-   bignum *a = malloc(N *sizeof(bignum));
-   char *results = malloc((N + 1) * sizeof(char));
-   double now, then;
-   double scost, pcost;
-      
-   initializeArray(results, N);
-   printf("%%%%%% Find all prime numbers in the range of 0 to %llu.\n", N);   
+#include <math.h>
  
-   then = currentTime();
-   computePrimes(results, 0, N);
-   now = currentTime();
-   scost = now - then;
-   printf("%%%%%% Serial code executiontime in second is %lf\n", scost);
-   printf("Total number of primes in that range is: %d.\n\n", arrSum(results, N + 1));
-
-   initializeArray(results, N);
-   then = currentTime();
-   pcomputePrimes(results, 0, N);
-   now = currentTime();
-   pcost = now - then;
-   printf("%%%%%% Parallel code executiontime with 4 threads in second is %lf\n", pcost);
-   printf("Total number of primes in that range is: %d.\n\n", arrSum(results, N + 1));
-   
-   printf("%%%%%% The speedup(SerialTimeCost / ParallelTimeCost) when using 4 threads is %lf\n", scost / pcost); 
-   printf("%%%%%% The efficiency(Speedup / NumProcessorCores) when using 4 threads is %lf\n", scost / pcost / 4); 
-
-   printArray(results, N);
-   return 0;
+// CUDA kernel. Each thread takes care of one element of c
+__global__ void vecAdd(double *a, double *b, double *c, int n)
+{
+    // Get our global thread ID
+    int id = blockIdx.x*blockDim.x+threadIdx.x;
+ 
+    // Make sure we do not go out of bounds
+    if (id < n)
+        c[id] = a[id] + b[id];
+}
+ 
+int main( int argc, char* argv[] )
+{
+    // Size of vectors
+    int n = 100000;
+ 
+    // Host input vectors
+    double *h_a;
+    double *h_b;
+    //Host output vector
+    double *h_c;
+ 
+    // Device input vectors
+    double *d_a;
+    double *d_b;
+    //Device output vector
+    double *d_c;
+ 
+    // Size, in bytes, of each vector
+    size_t bytes = n*sizeof(double);
+ 
+    // Allocate memory for each vector on host
+    h_a = (double*)malloc(bytes);
+    h_b = (double*)malloc(bytes);
+    h_c = (double*)malloc(bytes);
+ 
+    // Allocate memory for each vector on GPU
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+ 
+    int i;
+    // Initialize vectors on host
+    for( i = 0; i < n; i++ ) {
+        h_a[i] = sin(i)*sin(i);
+        h_b[i] = cos(i)*cos(i);
+    }
+ 
+    // Copy host vectors to device
+    cudaMemcpy( d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy( d_b, h_b, bytes, cudaMemcpyHostToDevice);
+ 
+    int blockSize, gridSize;
+ 
+    // Number of threads in each thread block
+    blockSize = 1024;
+ 
+    // Number of thread blocks in grid
+    gridSize = (int)ceil((float)n/blockSize);
+ 
+    // Execute the kernel
+    vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+ 
+    // Copy array back to host
+    cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );
+ 
+    // Sum up vector c and print result divided by n, this should equal 1 without error
+    double sum = 0;
+    for(i=0; i<n; i++)
+        sum += h_c[i];
+    printf("final result: %f\n", sum/n);
+ 
+    // Release device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+ 
+    // Release host memory
+    free(h_a);
+    free(h_b);
+    free(h_c);
+ 
+    return 0;
 }
