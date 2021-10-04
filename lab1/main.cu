3427dac11bccb026bbc3be1b95ac82f3cf0bda5e
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-04 09:35:52 -0700

    g

--- lab1/main.cu
+++ lab1/main.cu
@@ -185,10 +185,10 @@ int main( int argc, char* argv[] )
     cost_gpu = now_gpu - then_gpu;
 
     // Allocate for CPU proc
-    char *results = (char *)malloc((N + 1) * sizeof(char));
+    int *results = (int*)malloc(bytes);
     printArray(results, N);   
-    
-    initializeArray(results, N);
+
+    // initializeArray(results, N);
     printf("%%%%%% Find all prime numbers in the range of 3 to %llu.\n", N);   
   
     then_cpu = currentTime();
@@ -197,7 +197,7 @@ int main( int argc, char* argv[] )
 
     for(long i = 2; i < ceil(N/2); i++){
 		bignum num = (i * 2) - 1;
-		h_results[i*2]=0;
+		results[i*2]=0;
 		//is num prime
 		results[num] = h_isPrime(num);
 
