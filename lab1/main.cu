df0c6c969f375319a7ee000ac173152d74f2633d
aavdeyev1 <aavdeyev1@eagles.ewu.edu>
2021-10-05 09:20:14 -0700

    itu

--- lab1/main.cu
+++ lab1/main.cu
@@ -104,7 +104,7 @@ void initializeArray(char a[], bignum len){
     // init results char array
        bignum i;
        
-       for(i=3; i< len; i = i + 2)
+       for(i=3; i < len; i = i + 2)
        {
             a[i] = i;
             printf("here %llu [%llu]\n", i, a[i]);
